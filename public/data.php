af9f05191d7d1f97b896dfd68a1abe1cfa35c105
Anastasia Mal <lisanastya15@gmail.com>
2020-02-28 12:24:56 +0100

    added owners

--- public/data.php
+++ public/data.php
@@ -1,3 +1,23 @@
+<?php
+require_once 'DB.php';
+require_once 'DB_functions.php';
+
+DB::connect('localhost','clinic', 'root', 'rootroot');
+// DB::statement()
 $data = json_decode(file_get_contents('clients.json'), true);
 
-var_dump($data)
+foreach($data as $d){
+ $query = 
+ "INSERT
+  INTO `owners`
+  (`first_name` , `surname`)
+  VALUES
+  (?, ?)";
+
+  DB::insert($query, [
+    $d['first_name'], 
+    $d['surname']
+  ]);
+}
+
+ 
